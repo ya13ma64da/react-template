5bb467e181063d3561fa663c45f5849b434a09f3
Shin Yamada <ya13ma64da@gmail.com>
2026-03-08 18:32:52 +0900

    初期化

--- src/components/mine/icon.tsx
+++
@@ -1,5 +0,0 @@
-export function Icon({ className, onClick }: { className?: string; onClick?: () => void }) {
-  return (
-    <img src="/files/icons/icon.svg" onClick={onClick} alt="Icon" className={className} />
-  )
-}
--- src/lib/env.ts
+++
@@ -1,3 +0,0 @@
-export const env = {
-  title: import.meta.env.VITE_TITLE
-}
