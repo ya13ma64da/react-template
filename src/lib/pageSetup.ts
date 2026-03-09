7a6620b297ee53a0721296f8e1d572b6309d9881
Shin Yamada <ya13ma64da@gmail.com>
2026-03-10 00:11:40 +0900

    SEO対策追加 ルール修正 md追加

--- src/lib/pageSetup.ts
+++ src/lib/pageSetup.ts
@@ -1,7 +1,5 @@
-import { log } from "@/lib/log"
 import { env } from "@/lib/env"
 
 export function pageSetup(title: string) {
   document.title = `${title} - ${env.title}`
-  log(`User entered ${location.pathname} page.`)
 }
